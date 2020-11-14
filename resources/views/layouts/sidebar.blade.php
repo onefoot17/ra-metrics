67f5e997b17d30aa68645fe68585ab4ec3d213be
Felipe Pastana <felipe@vcompinc.com>
2020-11-13 19:10:18 -0500

    Added color-admin template

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,51 @@
+<!-- begin #sidebar -->
+<div id="sidebar" class="sidebar">
+    <!-- begin sidebar scrollbar -->
+    <div data-scrollbar="true" data-height="100%">
+        <!-- begin sidebar user -->
+        <ul class="nav">
+            <li class="nav-profile">
+                <a href="javascript:;" data-toggle="nav-profile">
+                    <div class="cover with-shadow"></div>
+                    <div class="image">
+                        <img src="../assets/img/user/user-13.jpg" alt="" />
+                    </div>
+                    <div class="info">
+                        <b class="caret pull-right"></b>Felipe Pastana
+                        <small>Back end developer</small>
+                    </div>
+                </a>
+            </li>
+            <li>
+                <ul class="nav nav-profile">
+                    <li><a href="javascript:;"><i class="fa fa-cog"></i> Settings</a></li>
+                    <li><a href="javascript:;"><i class="fa fa-pencil-alt"></i> Send Feedback</a></li>
+                    <li><a href="javascript:;"><i class="fa fa-question-circle"></i> Helps</a></li>
+                </ul>
+            </li>
+        </ul>
+        <!-- end sidebar user -->
+        <!-- begin sidebar nav -->
+        <ul class="nav"><li class="nav-header">Navigation</li>            
+            
+            <li class="has-sub active">
+                <a href="javascript:;">
+                    <b class="caret"></b>
+                    <i class="fa fa-cogs"></i>
+                    <span>Admin <span class="label label-theme">NEW</span></span>
+                </a>
+                <ul class="sub-menu">
+                    <li><a href="page_blank.html">Blank Page</a></li>
+                    <li class="active"><a href="page_with_footer.html">Caregiver List</a></li>
+                </ul>
+            </li>
+            <!-- begin sidebar minify button -->
+            <li><a href="javascript:;" class="sidebar-minify-btn" data-click="sidebar-minify"><i class="fa fa-angle-double-left"></i></a></li>
+            <!-- end sidebar minify button -->
+        </ul>
+        <!-- end sidebar nav -->
+    </div>
+    <!-- end sidebar scrollbar -->
+</div>
+<div class="sidebar-bg"></div>
+<!-- end #sidebar -->
