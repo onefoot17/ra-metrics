e3d814ef375d641efc8e0f8c92c487787b82a669
Felipe Pastana <felipe@vcompinc.com>
2020-12-01 18:26:00 -0500

    added some itens to the dashboard of the system

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -89,42 +89,42 @@
         <!-- Right bar overlay-->
         <div class="rightbar-overlay"></div>
 
+        <!-- Vendor js -->
+        <script src="{{asset('ubold/assets/js/vendor.min.js')}}"></script>
+
+        <!-- Tost-->
+        <script src="{{asset('ubold/assets/libs/jquery-toast-plugin/jquery.toast.min.js')}}"></script>
+
         @section('js')
-
-            <!-- Vendor js -->
-            <script src="{{asset('ubold/assets/js/vendor.min.js')}}"></script>
-
-            <!-- Tost-->
-            <script src="{{asset('ubold/assets/libs/jquery-toast-plugin/jquery.toast.min.js')}}"></script>
-
-            <!-- App js -->
-            <script src="{{asset('ubold/assets/js/app.min.js')}}"></script>
-
-            <script>
-                !(function (p) {
-                "use strict";
-                function t() {}
-                (t.prototype.send = function (t, i, o, e, n, a, s, r) {
-                    var c = { heading: t, text: i, position: o, loaderBg: e, icon: n, hideAfter: (a = a || 3e3), stack: (s = s || 1) };
-                    r && (c.showHideTransition = r), console.log(c), p.toast().reset("all"), p.toast(c);
-                }),
-                    (p.NotificationApp = new t()),
-                    (p.NotificationApp.Constructor = t);
-                })(window.jQuery),
-                (function (i) {
-                    "use strict";
-
-                    @if(!is_null(session('message-success')))
-                        i.NotificationApp.send("Well Done!", "{{session('message-success')}}", "top-right", "#5ba035", "success");
-                    @elseif(!is_null(session('message-error')))
-                        i.NotificationApp.send("Oh snap!", "{{session('message-error')}}", "top-right", "#bf441d", "error");
-                    @elseif(!is_null(session('message-warning')))
-                        i.NotificationApp.send("Heads up!", "{{session('message-warning')}}", "top-center", "#da8609", "warning");
-                    @endif
-                })(window.jQuery);
-            </script>
-
+        
         @show
+
+        <!-- App js -->
+        <script src="{{asset('ubold/assets/js/app.min.js')}}"></script>
+
+        <script>
+            !(function (p) {
+            "use strict";
+            function t() {}
+            (t.prototype.send = function (t, i, o, e, n, a, s, r) {
+                var c = { heading: t, text: i, position: o, loaderBg: e, icon: n, hideAfter: (a = a || 3e3), stack: (s = s || 1) };
+                r && (c.showHideTransition = r), console.log(c), p.toast().reset("all"), p.toast(c);
+            }),
+                (p.NotificationApp = new t()),
+                (p.NotificationApp.Constructor = t);
+            })(window.jQuery),
+            (function (i) {
+                "use strict";
+
+                @if(!is_null(session('message-success')))
+                    i.NotificationApp.send("Well Done!", "{{session('message-success')}}", "top-right", "#5ba035", "success");
+                @elseif(!is_null(session('message-error')))
+                    i.NotificationApp.send("Oh snap!", "{{session('message-error')}}", "top-right", "#bf441d", "error");
+                @elseif(!is_null(session('message-warning')))
+                    i.NotificationApp.send("Heads up!", "{{session('message-warning')}}", "top-center", "#da8609", "warning");
+                @endif
+            })(window.jQuery);
+        </script>
         
     </body>
 </html>
--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -13,23 +13,338 @@
 @section('content')
 
     <div class="row">
+        <div class="col-md-6 col-xl-3">
+            <div class="widget-rounded-circle card-box">
+                <div class="row">
+                    <div class="col-6">
+                        <div class="avatar-lg rounded-circle bg-soft-primary border-primary border">
+                            <i class="fe-heart font-22 avatar-title text-primary"></i>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="text-right">
+                            <h3 class="mt-1"><span data-plugin="counterup">548</span></h3>
+                            <p class="text-muted mb-1 text-truncate">@lang('Plant Parents / Species')</p>
+                        </div>
+                    </div>
+                </div> <!-- end row-->
+            </div> <!-- end widget-rounded-circle-->
+        </div> <!-- end col-->
 
-        <div class="col-lg-12">
-            <div class="card-box">
-                <h4 class="header-title">Plants</h4>
+        <div class="col-md-6 col-xl-3">
+            <div class="widget-rounded-circle card-box">
+                <div class="row">
+                    <div class="col-6">
+                        <div class="avatar-lg rounded-circle bg-soft-success border-success border">
+                            <i class="fe-shopping-cart font-22 avatar-title text-success"></i>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="text-right">
+                            <h3 class="text-dark mt-1"><span data-plugin="counterup">214</span></h3>
+                            <p class="text-muted mb-1 text-truncate">@lang('Plant Types')</p>
+                        </div>
+                    </div>
+                </div> <!-- end row-->
+            </div> <!-- end widget-rounded-circle-->
+        </div> <!-- end col-->
 
-                <div class="badge badge-secondary mb-3">Ongoing</div>
+        <div class="col-md-6 col-xl-3">
+            <div class="widget-rounded-circle card-box">
+                <div class="row">
+                    <div class="col-6">
+                        <div class="avatar-lg rounded-circle bg-soft-info border-info border">
+                            <i class="fe-bar-chart-line- font-22 avatar-title text-info"></i>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="text-right">
+                            <h3 class="text-dark mt-1"><span data-plugin="counterup">1009</span></h3>
+                            <p class="text-muted mb-1 text-truncate">@lang('Plants')</p>
+                        </div>
+                    </div>
+                </div> <!-- end row-->
+            </div> <!-- end widget-rounded-circle-->
+        </div> <!-- end col-->
 
-                <h6>Project Overview:</h6>
-
-                <p class="text-muted mb-2">
-                    Panel content here.
-                </p>
-
-            </div> <!-- end card-box -->
-        </div> <!-- end col -->
+        <div class="col-md-6 col-xl-3">
+            <div class="widget-rounded-circle card-box">
+                <div class="row">
+                    <div class="col-6">
+                        <div class="avatar-lg rounded-circle bg-soft-warning border-warning border">
+                            <i class="fe-eye font-22 avatar-title text-warning"></i>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="text-right">
+                            <h3 class="text-dark mt-1"><span data-plugin="counterup">97</span></h3>
+                            <p class="text-muted mb-1 text-truncate">@lang('Participants')</p>
+                        </div>
+                    </div>
+                </div> <!-- end row-->
+            </div> <!-- end widget-rounded-circle-->
+        </div> <!-- end col-->
     </div>
-    <!-- end row -->    
+    <!-- end row-->
+
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="card-widgets">
+                        <a href="javascript: void(0);" data-toggle="reload"><i class="mdi mdi-refresh"></i></a>
+                        <a data-toggle="collapse" href="#cardCollpase5" role="button" aria-expanded="false" aria-controls="cardCollpase5"><i class="mdi mdi-minus"></i></a>
+                        <a href="javascript: void(0);" data-toggle="remove"><i class="mdi mdi-close"></i></a>
+                    </div>
+                    <h4 class="header-title mb-0">Top Selling Products</h4>
+
+                    <div id="cardCollpase5" class="collapse pt-3 show">
+                        <div class="table-responsive">
+                            <table class="table table-hover table-centered mb-0">
+                                <thead>
+                                    <tr>
+                                        <th>Product Name</th>
+                                        <th>Price</th>
+                                        <th>Quantity</th>
+                                        <th>Amount</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>ASOS Ridley High Waist</td>
+                                        <td>$79.49</td>
+                                        <td>82</td>
+                                        <td>$6,518.18</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Marco Lightweight Shirt</td>
+                                        <td>$128.50</td>
+                                        <td>37</td>
+                                        <td>$4,754.50</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Half Sleeve Shirt</td>
+                                        <td>$39.99</td>
+                                        <td>64</td>
+                                        <td>$2,559.36</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Lightweight Jacket</td>
+                                        <td>$20.00</td>
+                                        <td>184</td>
+                                        <td>$3,680.00</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Marco Shoes</td>
+                                        <td>$28.49</td>
+                                        <td>69</td>
+                                        <td>$1,965.81</td>
+                                    </tr>
+                                    <tr>
+                                        <td>ASOS Ridley High Waist</td>
+                                        <td>$79.49</td>
+                                        <td>82</td>
+                                        <td>$6,518.18</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Half Sleeve Shirt</td>
+                                        <td>$39.99</td>
+                                        <td>64</td>
+                                        <td>$2,559.36</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Lightweight Jacket</td>
+                                        <td>$20.00</td>
+                                        <td>184</td>
+                                        <td>$3,680.00</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div> <!-- end table responsive-->
+                    </div> <!-- collapsed end -->
+                </div> <!-- end card-body -->
+            </div> <!-- end card-->
+        </div> <!-- end col -->
+        <div class="col-lg-6">
+            <div class="row">
+                <div class="col-lg-6">
+                    <!-- Portlet card -->
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="card-widgets">
+                                <a href="javascript: void(0);" data-toggle="reload"><i class="mdi mdi-refresh"></i></a>
+                                <a data-toggle="collapse" href="#cardCollpase1" role="button" aria-expanded="false" aria-controls="cardCollpase1"><i class="mdi mdi-minus"></i></a>
+                                <a href="javascript: void(0);" data-toggle="remove"><i class="mdi mdi-close"></i></a>
+                            </div>
+                            <h4 class="header-title mb-0">Lifetime Sales</h4>
+        
+                            <div id="cardCollpase1" class="collapse pt-3 show">
+                                <div class="text-center">
+                                    <div id="lifetime-sales" data-colors="#00acc1,#f1556c"></div>
+        
+                                    <div class="row mt-3">
+                                        <div class="col-4">
+                                            <p class="text-muted font-15 mb-1 text-truncate">Target</p>
+                                            <h4><i class="fe-arrow-down text-danger mr-1"></i>$7.8k</h4>
+                                        </div>
+                                        <div class="col-4">
+                                            <p class="text-muted font-15 mb-1 text-truncate">Last week</p>
+                                            <h4><i class="fe-arrow-up text-success mr-1"></i>$1.4k</h4>
+                                        </div>
+                                        <div class="col-4">
+                                            <p class="text-muted font-15 mb-1 text-truncate">Last Month</p>
+                                            <h4><i class="fe-arrow-down text-danger mr-1"></i>$9.8k</h4>
+                                        </div>
+                                    </div> <!-- end row -->
+                                    
+                                </div>
+                            </div> <!-- collapsed end -->
+                        </div> <!-- end card-body -->
+                    </div> <!-- end card-->
+                </div> <!-- end col-->
+        
+                <div class="col-lg-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="card-widgets">
+                                <a href="javascript: void(0);" data-toggle="reload"><i class="mdi mdi-refresh"></i></a>
+                                <a data-toggle="collapse" href="#cardCollpase2" role="button" aria-expanded="false" aria-controls="cardCollpase2"><i class="mdi mdi-minus"></i></a>
+                                <a href="javascript: void(0);" data-toggle="remove"><i class="mdi mdi-close"></i></a>
+                            </div>
+                            <h4 class="header-title mb-0">Income Amounts</h4>
+        
+                            <div id="cardCollpase2" class="collapse pt-3 show">
+                                <div class="text-center">
+                                    <div id="income-amounts" data-colors="#00acc1"></div>
+                                    <div class="row mt-3">
+                                        <div class="col-4">
+                                            <p class="text-muted font-15 mb-1 text-truncate">Target</p>
+                                            <h4><i class="fe-arrow-up text-success mr-1"></i>641</h4>
+                                        </div>
+                                        <div class="col-4">
+                                            <p class="text-muted font-15 mb-1 text-truncate">Last week</p>
+                                            <h4><i class="fe-arrow-down text-danger mr-1"></i>234</h4>
+                                        </div>
+                                        <div class="col-4">
+                                            <p class="text-muted font-15 mb-1 text-truncate">Last Month</p>
+                                            <h4><i class="fe-arrow-up text-success mr-1"></i>3201</h4>
+                                        </div>
+                                    </div> <!-- end row -->
+                                </div>
+                            </div> <!-- collapsed end -->
+                        </div> <!-- end card-body -->
+                    </div> <!-- end card-->
+                </div> <!-- end col-->
+            </div>
+            <!-- end row -->
+        </div>
+    </div>
+    <!-- end row -->   
+
+    <div class="row">
+        <div class="col-lg-4">
+            <!-- Portlet card -->
+            <div class="card">
+                <div class="card-body">
+                    <div class="card-widgets">
+                        <a href="javascript: void(0);" data-toggle="reload"><i class="mdi mdi-refresh"></i></a>
+                        <a data-toggle="collapse" href="#cardCollpase1" role="button" aria-expanded="false" aria-controls="cardCollpase1"><i class="mdi mdi-minus"></i></a>
+                        <a href="javascript: void(0);" data-toggle="remove"><i class="mdi mdi-close"></i></a>
+                    </div>
+                    <h4 class="header-title mb-0">Lifetime Sales</h4>
+
+                    <div id="cardCollpase1" class="collapse pt-3 show">
+                        <div class="text-center">
+                            <div id="lifetime-sales" data-colors="#00acc1,#f1556c"></div>
+
+                            <div class="row mt-3">
+                                <div class="col-4">
+                                    <p class="text-muted font-15 mb-1 text-truncate">Target</p>
+                                    <h4><i class="fe-arrow-down text-danger mr-1"></i>$7.8k</h4>
+                                </div>
+                                <div class="col-4">
+                                    <p class="text-muted font-15 mb-1 text-truncate">Last week</p>
+                                    <h4><i class="fe-arrow-up text-success mr-1"></i>$1.4k</h4>
+                                </div>
+                                <div class="col-4">
+                                    <p class="text-muted font-15 mb-1 text-truncate">Last Month</p>
+                                    <h4><i class="fe-arrow-down text-danger mr-1"></i>$9.8k</h4>
+                                </div>
+                            </div> <!-- end row -->
+                            
+                        </div>
+                    </div> <!-- collapsed end -->
+                </div> <!-- end card-body -->
+            </div> <!-- end card-->
+        </div> <!-- end col-->
+
+        <div class="col-lg-4">
+            <div class="card">
+                <div class="card-body">
+                    <div class="card-widgets">
+                        <a href="javascript: void(0);" data-toggle="reload"><i class="mdi mdi-refresh"></i></a>
+                        <a data-toggle="collapse" href="#cardCollpase2" role="button" aria-expanded="false" aria-controls="cardCollpase2"><i class="mdi mdi-minus"></i></a>
+                        <a href="javascript: void(0);" data-toggle="remove"><i class="mdi mdi-close"></i></a>
+                    </div>
+                    <h4 class="header-title mb-0">Income Amounts</h4>
+
+                    <div id="cardCollpase2" class="collapse pt-3 show">
+                        <div class="text-center">
+                            <div id="income-amounts" data-colors="#00acc1"></div>
+                            <div class="row mt-3">
+                                <div class="col-4">
+                                    <p class="text-muted font-15 mb-1 text-truncate">Target</p>
+                                    <h4><i class="fe-arrow-up text-success mr-1"></i>641</h4>
+                                </div>
+                                <div class="col-4">
+                                    <p class="text-muted font-15 mb-1 text-truncate">Last week</p>
+                                    <h4><i class="fe-arrow-down text-danger mr-1"></i>234</h4>
+                                </div>
+                                <div class="col-4">
+                                    <p class="text-muted font-15 mb-1 text-truncate">Last Month</p>
+                                    <h4><i class="fe-arrow-up text-success mr-1"></i>3201</h4>
+                                </div>
+                            </div> <!-- end row -->
+                        </div>
+                    </div> <!-- collapsed end -->
+                </div> <!-- end card-body -->
+            </div> <!-- end card-->
+        </div> <!-- end col-->
+
+        <div class="col-lg-4">
+            <div class="card">
+                <div class="card-body">
+                    <div class="card-widgets">
+                        <a href="javascript: void(0);" data-toggle="reload"><i class="mdi mdi-refresh"></i></a>
+                        <a data-toggle="collapse" href="#cardCollpase3" role="button" aria-expanded="false" aria-controls="cardCollpase3"><i class="mdi mdi-minus"></i></a>
+                        <a href="javascript: void(0);" data-toggle="remove"><i class="mdi mdi-close"></i></a>
+                    </div>
+                    <h4 class="header-title mb-0">Total Users</h4>
+
+                    <div id="cardCollpase3" class="collapse pt-3 show">
+                        <div class="text-center">
+                            <div id="total-users" data-colors="#00acc1,#4b88e4,#e3eaef,#fd7e14"></div>
+                            <div class="row mt-3">
+                                <div class="col-4">
+                                    <p class="text-muted font-15 mb-1 text-truncate">Target</p>
+                                    <h4><i class="fe-arrow-down text-danger mr-1"></i>18k</h4>
+                                </div>
+                                <div class="col-4">
+                                    <p class="text-muted font-15 mb-1 text-truncate">Last week</p>
+                                    <h4><i class="fe-arrow-up text-success mr-1"></i>3.25k</h4>
+                                </div>
+                                <div class="col-4">
+                                    <p class="text-muted font-15 mb-1 text-truncate">Last Month</p>
+                                    <h4><i class="fe-arrow-up text-success mr-1"></i>28k</h4>
+                                </div>
+                            </div> <!-- end row -->
+                        </div>
+                    </div> <!-- collapsed end -->
+                </div> <!-- end card-body -->
+            </div> <!-- end card-->
+        </div> <!-- end col-->
+    </div>
+    <!-- end row -->
 
 @stop
 
