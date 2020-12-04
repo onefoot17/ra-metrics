469a72969ec469dc492ad376bc4171af9f959028
Felipe Pastana <felipe@vcompinc.com>
2020-12-04 09:10:40 -0500

    Removed the gear to show options

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -309,11 +309,11 @@
                 </div>
             </li>
 
-            <li class="dropdown notification-list">
+            {{-- <li class="dropdown notification-list">
                 <a href="javascript:void(0);" class="nav-link right-bar-toggle waves-effect waves-light">
                     <i class="fe-settings noti-icon"></i>
                 </a>
-            </li>
+            </li> --}}
 
         </ul>
 
