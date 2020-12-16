b00ca204f80809d87847512382f4331837171635
Earl Misquitta <earl@vcompinc.com>
2020-12-15 19:34:20 -0500

    Change Website template to Porto pt 3

---
+++ resources/views/admin/layouts/footerscripts.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <script>document.write(new Date().getFullYear())</script> &copy; {{env('APP_NAME')}} by <a href="https://vcompinc.com" target='_blank' class="text-white-50">VCOMP Inc.</a>
+</footer>
+
+<!-- Vendor js -->
+<script src="{{ asset( 'admin/assets/js/vendor.min.js' ) }}"></script>
+
+<!-- App js -->
+<script src="{{ asset( 'admin/assets/js/app.min.js' ) }}"></script>
